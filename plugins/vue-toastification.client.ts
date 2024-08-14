00850f43d96d1a4a19a3c303ef4b2c81f92bd38c
kitchanut <kitchanut.r@gmail.com>
2024-08-14 16:26:21 +0700

    Fix Auth

--- plugins/vue-toastification.client.ts
+++ plugins/vue-toastification.client.ts
@@ -5,7 +5,7 @@ import "vue-toastification/dist/index.css";
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(vt.default, {
         // position: "bottom-center",
-        position: "top-center",
+        position: "top-right",
         hideProgressBar: true,
         closeButton: false,
         timeout: 1500,
